b661c66e6f11361c0f74a85e7067d40c94b40804
Austin Adams <aadams1@einsteinium.cs.dixie.edu>
2016-07-14 15:51:53 -0600

    bootstrap

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -10,13 +10,11 @@ var AboutPage = React.createClass({
 				<h1>About Swoop</h1>
 				<p>We are going to change the way people communicate</p>
 				<ul>
-					<li>Gulp</li>
-					<li>React</li>
-					<li>React Router</li>
-					<li>Node</li>
-					<li>Browserify</li>
-					<li>Bootstrap</li>
-					<li>Flux</li>
+					<li>Jake</li>
+					<li>Forgive Us</li>
+					<li>As Jesus Forgave the Jews</li>
+					<li>For we did not know what we had</li>
+					<li>Until it was Gone</li>
 				</ul>
 			</div>
 		);
