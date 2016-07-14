b6cd48aa482dac11f5b4e6c7fbd0aa21b085c7be
Tech Camp 107 <dixie107@fermium.cs.dixie.edu>
2016-07-14 14:53:38 -0600

    Put the todo list underneath the home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,35 +3,49 @@
 var React = require('react');
 var TodoForm = require('./todos/TodoForm');
 var TextInput = require('./common/TextInput');
+var Link = require('react-router').Link;
+var TodoList = require('./todos/TodoList');
+var TodoStore = require('../stores/todoStore');
+
 
 
 var Home = React.createClass({
+	getInitialState: function () {
+		return {
+			todos: TodoStore.getAllTodos()
+		}
+	},
+
+	componentWillMount: function () {
+		TodoStore.addChangeListener(this.onChange);
+	},
+
+	componentWillUnmount: function () {
+		TodoStore.removeChangeListener(this.onChange);
+	},
+
+	onChange: function () {
+		this.setState({
+			todos: TodoStore.getAllTodos()
+		});
+	},
+
 	render: function () {
 		return (
 			<div className="jumbotron">
 				<h1>Welcome To Swoop</h1>
 				<p>Lets post some stuff</p>
+				<TodoList
+					todos={this.state.todos}
+					/>
 			</div>
-		);
-	
-		console.log(this.props.errors);
-		return (
-			<form onSubmit={this.props.saveTodo}>
-				<h3>Todo Form</h3>
-				<TextInput
-					name="post"
-					placeholder="Post"
-					value={this.props.todo.post}
-					saveTodoState={this.props.saveTodoState}
-					error={this.props.errors.post}
-				/>
-				<input type="submit" value="Save Todo" className="btn btn-success btn-lg" />
-			</form>
 		);
 	}
 	
 
 });
+
+
 
 
 module.exports = Home;
